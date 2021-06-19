2d3ba1f9b7f5cf2e32c992da06ec4257ea43e822
wgj <1031403310@qq.com>
2021-06-19 18:35:00 +0800

    hhha

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+package src;
+
+/**
+ * @Author wgj
+ * @create 2021-06-2021/6/19 18:31
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
